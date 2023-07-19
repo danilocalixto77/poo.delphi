6ffa41709ea808075970ff810b7fe22232e1b671
danilocalixto77 <danilocalixto@gmail.com>
2023-07-19 18:39:25 -0400

    202307191839

---
+++ aula04/Aula04.dpr
@@ -0,0 +1,15 @@
+program Aula04;
+
+uses
+  Vcl.Forms,
+  Forms.FormPrincipal in 'Forms.FormPrincipal.pas' {FormPrincipal},
+  Classes.Exemplo in 'src\Classes.Exemplo.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.Run;
+end.
